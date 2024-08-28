6604374ebf3ec3b0f2e67cfc33c9b565b72db1b8
hellenilda <hellen95limaaraujo@gmail.com>
2024-08-28 10:22:14 -0300

    fix: type do parâmetro de get_curso

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ async def get_cursos():
     return cursos
 
 @app.get('/cursos/{curso_id}')
-async def get_curso(curso_id):
-    curso = cursos(curso_id)
+async def get_curso(curso_id: int):
+    curso = cursos[curso_id]
     curso.update({"id": curso_id})
     
     return curso
