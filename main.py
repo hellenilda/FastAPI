f2ab7e44e24d972203d499e37a48dfe53092dbd0
hellenilda <hellen95limaaraujo@gmail.com>
2024-08-28 09:25:58 -0300

    feat: dicionário de cursos

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-async def raiz():
-    return {"msg":"Estudos de FastAPI"}
+cursos = {
+    1: {
+        "titulo": "FastAPI - APIs Modernas e Assíncronas com Python",
+        "aulas": 112,
+        "horas": 58
+    },
+    2: {
+        "titulo": "Algoritmos e Lógica de Programação",
+        "aulas": 85,
+        "horas": 23
+    },
+    3: {
+        "titulo": "Machine Learning com AWS e Sagemaker",
+        "aulas": 86,
+        "horas": 16
+    }
+}
